297b998c29cd364dba8d3983d7513dbbfd67d2d7
Satish Samala <satish.samala@gmail.com>
2024-12-18 16:11:58 +0530

    login and registration

---
+++ app/Http/Controllers/AuthManager.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthManager extends Controller
+{
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function register()
+    {
+        return view('auth.register');
+    }
+
+    public function loginPost(Request $request)
+    {
+        // Validate the request...
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended(route('home'));
+        }
+
+        return redirect('login')->with('error', 'Invalid Email or Password');
+    }
+
+
+    function registerPost(Request $request)
+    {
+        // Validate the request...
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = new User();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        if ($user->save()) {
+            return redirect(route('login'))->with('success', 'User created successfully');
+        } else {
+            return redirect(route('register'))->with('error', 'Failed to create user');
+        }
+    }
+
+    function logout()
+    {
+        Auth::logout();
+
+        return redirect('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\HelloWorldController;
+use App\Http\Controllers\AuthManager;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/hello-world', [HelloWorldController::class, 'index']);
+
+Route::get('/login', [AuthManager::class, 'login'])->name('login');
+Route::get('/register', [AuthManager::class, 'register'])->name('register');
+
+Route::post('/login', [AuthManager::class, 'loginPost'])->name('login.post');
+Route::post('/register', [AuthManager::class, 'registerPost'])->name('register.post');
+
+Route::get('logout', [AuthManager::class, 'loginPost'])->name('logout');
